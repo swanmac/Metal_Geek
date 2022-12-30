8c3648cc4dd08d2c1690dd5f834476d35aa04887
Greg Swan <gmswan99@gmail.com>
2022-12-30 09:29:36 -0500

    remove delete button

--- metal-geek/src/components/ArtistDetails.jsx
+++ metal-geek/src/components/ArtistDetails.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import DeleteBtn from "./DeleteBtn.jsx"
 import { Link } from "react-router-dom";
 
 export default function ArtistDetails () {
