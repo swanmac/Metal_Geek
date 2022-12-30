17ce53e64e067bfbbcfdcd3dea3c174bd394e685
Greg Swan <gmswan99@gmail.com>
2022-12-30 12:01:45 -0500

    created update artist

---
+++ metal-geek/src/components/UpdateArtist.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { baseURL, headers } from "./../services/artist.service";
+
+export const UpdateArtist = () => {
+}
