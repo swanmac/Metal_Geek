8afb0d9d629145c9a4404c54324d698f1ea0b6f7
Greg Swan <gmswan99@gmail.com>
2023-01-01 20:51:01 -0500

    update footer links

--- metal-geek/src/components/Footer.jsx
+++ metal-geek/src/components/Footer.jsx
@@ -4,9 +4,13 @@ export default function Footer () {
             <div className="footer-copyright">
             <p>© Metal Geek</p>
             </div>
-
+            <div className="footer">
+            <p className="color1"></p> 
             <p> <a href="https://github.com/swanmac" target=" _blank" rel="noopener noreferrer">GitHub-Greg</a></p>
+
+            <p className="color2"></p> 
+            <p> <a href="https://www.linkedin.com/in/gregoryswan/" target=" _blank" rel="noopener noreferrer">Linkedin</a></p>
         </div>
-        
+        </div>
     )
 }
