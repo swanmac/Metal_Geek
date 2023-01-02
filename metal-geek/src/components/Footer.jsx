9497a3f395c09c6067b1061d478476545cfa7668
Greg Swan <gmswan99@gmail.com>
2023-01-01 21:22:03 -0500

    added styling to footer

--- metal-geek/src/components/Footer.jsx
+++ metal-geek/src/components/Footer.jsx
@@ -5,10 +5,8 @@ export default function Footer () {
             <p>© Metal Geek</p>
             </div>
             <div className="footer">
-            <p className="color1"></p> 
-            <p> <a href="https://github.com/swanmac" target=" _blank" rel="noopener noreferrer">GitHub-Greg</a></p>
+            <p> <a href="https://github.com/swanmac" target=" _blank" rel="noopener noreferrer">GitHub</a></p>
 
-            <p className="color2"></p> 
             <p> <a href="https://www.linkedin.com/in/gregoryswan/" target=" _blank" rel="noopener noreferrer">Linkedin</a></p>
         </div>
         </div>
