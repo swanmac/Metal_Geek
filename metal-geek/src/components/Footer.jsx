d91e31336656210ca577597dbfc08c0d2baec73d
Greg Swan <gmswan99@gmail.com>
2023-01-05 18:42:40 -0500

    updated footer tailwind

--- metal-geek/src/components/Footer.jsx
+++ metal-geek/src/components/Footer.jsx
@@ -2,16 +2,20 @@ export default function Footer () {
     return (
         <div className="w-full mt-40 bg-zinc-400 py-y px-2">
         <div className="footer-container">
-            <div className="footer-copyright">
-            <p>© Metal Geek</p>
-            </div>
+            
             <br />
             <div className="footer">
             <a href="https://github.com/swanmac" target=" _blank" className="github">GitHub</a>
 
+            <a href="https://github.com/swanmac/Metal_Geek/tree/main/metal-geek" target=" _blank" className="repo">Repository</a>
+
             <div className="social"> <a href="https://www.linkedin.com/in/gregoryswan/" target=" _blank" className="social">LinkedIn</a>
         </div>
         </div>
+        <br />
+        <div className="footer-copyright">
+            <p>© Metal Geek</p>
+            </div>
     </div>
     </div>
     )
